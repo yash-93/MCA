3c786c196d237e52575bad8713b1d18be53a23c2
yash-93 <yashdeepbachhas93@gmail.com>
2019-02-12 20:12:44 +0530

    menu created

--- SEM II/DFS/stack.c
+++ SEM II/DFS/stack.c
@@ -1,1 +1,25 @@
+#include<stdio>
 
+void push(int ele, int top, int size, int ar[size]){
+    if(top > (size - 1))
+        printf("Overflow");
+    else
+        ++top;
+        ar[top] = ele;
+}
+
+int pop(int top, int size, int ar[size]){
+    if(top == -1)
+        printf("Underflow");
+    else
+        return ar[top];
+}
+
+int main(){
+    int size = 10, top = -1;
+    printf("MENU");
+    printf("1. INSERT");
+    printf("2. DELETE");
+    printf("3. VIEW");
+    return 0;
+}
