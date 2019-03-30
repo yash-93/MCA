4e11a146d0191f3e085f75c0c327ba0510b0b47c
yash-93 <yashdeepbachhas93@gmail.com>
2019-03-30 12:50:48 +0530

    Operator Overloading Created

---
+++ SEM II/CPP/Operator Overloading/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Increment {
+    int x,y;
+
+public:
+    void getdata(int a, int b){
+        x = a;
+        y = b;
+    }
+
+    void display(){
+        cout << x << " " <<y;
+    }
+
+    void operator++();
+};
+
+void Increment :: operator++(){
+    x = ++x;
+    y = ++y;
+}
+
+int main()
+{
+    Increment o;
+    o.getdata(5, 6);
+    o.display();
+    ++o;
+    cout << endl;
+    o.display();
+    return 0;
+}
