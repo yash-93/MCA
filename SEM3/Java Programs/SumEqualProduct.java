06c12265ab875dd44e7a76f25c520e60bf141b6e
yash-93 <yashdeepbachhas93@gmail.com>
2019-09-14 19:33:04 +0530

    SumEqualProduct completed

--- SEM3/Java Programs/SumEqualProduct.java
+++ SEM3/Java Programs/SumEqualProduct.java
@@ -1,5 +1,35 @@
 class SumEqualProduct{
 	public static void main(String[] args) {
-		
+		int nums = 100, numl = 1000, sum = 0, mul = 1;
+
+		while (nums < 1000){
+			for (int n = nums ; n!=0 ; n/=10) {
+				sum += n%10;
+				mul *= n%10;
+			}
+			if(sum == mul){
+				System.out.println("Smallest number with equal sum and product is : " + nums);
+				break;
+			} else {
+				sum = 0;
+				mul = 1;
+				++nums;
+			}
+		}
+
+		while (numl > 99){
+			for (int n = numl ; n!=0 ; n/=10) {
+				sum += n%10;
+				mul *= n%10;
+			}
+			if(sum == mul){
+				System.out.println("Biggest number with equal sum and product is : " + numl);
+				break;
+			} else {
+				sum = 0;
+				mul = 1;
+				--numl;
+			}
+		}
 	}
 }
