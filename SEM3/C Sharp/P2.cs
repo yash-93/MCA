271c02bf7fde2d803e890a033e257298c6339412
yash-93 <yashdeepbachhas93@gmail.com>
2019-09-22 12:50:02 +0530

    C Sharp started

---
+++ SEM3/C Sharp/P2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practicals
+{
+	public class MyClass
+	{
+		static void Main(string[] args){
+			string fname, sname = "Bachhas";
+			Console.Write("Enter your first name : ");
+			fname = Console.ReadLine();
+			Console.WriteLine("Your second name is : " + sname);
+			Console.Write("Your full name is : " + fname + " " + sname);
+			Console.ReadKey();
+		}
+	}
+}
+
