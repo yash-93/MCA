5a50eceab6306360797259dd6f4c1831d3da7a8c
yash-93 <yashdeepbachhas93@gmail.com>
2019-09-22 20:27:45 +0530

    fibonacci

---
+++ SEM3/C Sharp/P6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practicals
+{
+	public class MyClass
+	{
+		public static int fib(int a){
+			if (a <= 1) {
+				return a;
+			} else {
+				return fib(a - 1)+fib(a - 2);
+			}
+		}
+
+		static void Main(string[] args){
+			Console.Write ("Enter an integer integer : ");
+			int num = Convert.ToInt32 (Console.ReadLine ());
+
+			//WITH RECURSION
+			Console.WriteLine ("\nWITH RECURSION");
+			for(int i = 0; i < num; i++){
+				Console.WriteLine (" " + fib(i));
+			}
+		}
+	}
+}
+
