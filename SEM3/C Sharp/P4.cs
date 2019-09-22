b4bc161e18cf1baa3f2f2e366e98215de8954cd3
yash-93 <yashdeepbachhas93@gmail.com>
2019-09-22 17:58:07 +0530

    Integer Operations

---
+++ SEM3/C Sharp/P4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Practicals
+{
+	public class MyClass
+	{
+		static void Main(string[] args){
+			string val, rev = "";
+			int num, len, sum = 0, index = 1;
+			Console.Write("Enter an Integer : ");
+			val = Console.ReadLine();
+			len = val.Length;
+			num = Convert.ToInt32(val);
+		
+		//USING FOR LOOP
+			Console.WriteLine ("USING FOR LOOP");
+			Console.WriteLine ("Table of " + val + " is");
+
+			for(int i = 1 ; i <= 10 ; i++ ){
+				Console.WriteLine(num + " * " + i + " = " + num*i);
+			}
+
+			for(int i = 0 ; i < len ; i++ ){
+				int value = num;
+				int rr = value % 10;
+				sum += rr;
+				rev += rr;
+				value = value / 10;
+			}
+
+			Console.WriteLine("Reversed number is : " + rev);
+			Console.WriteLine("Sum of digits of number is : " + sum);
+		
+		//USING DO WHILE LOOP
+			Console.WriteLine ("\nUSING DO WHILE LOOP\n");
+			Console.WriteLine ("Table of " + val + " is");
+			do{
+				Console.WriteLine(num + " * " + index + " = " + num*index);
+				++index;
+			}while(index<=10);
+			index = sum = 0;
+			rev = @"";
+			do{
+				int rr = num % 10;
+				sum += rr;
+				rev += rr;
+				num = num / 10;
+				++index;
+			}while(index < len);
+			Console.WriteLine("Reversed number is : " + rev);
+			Console.WriteLine("Sum of digits of number is : " + sum);
+
+		}
+	}
+}
+
