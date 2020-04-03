08d85dff2cf8f8b889abe5da153d881eb3d2325a
yash-93 <yashdeepbachhas93@gmail.com>
2020-04-03 18:25:07 +0530

    Fibonacci with Dynamic Programming added

---
+++ SEM 4/Dynamic Programming/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "dynamicprogramming.h"
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the positive integer : ";
+    cin >> num;
+    cout << fib(num);
+
+    return 0;
+}
